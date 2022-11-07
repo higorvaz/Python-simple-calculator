71eacf72dfb971d5ed000964d9a86c9ca949aea5
Higor Câmara Vaz da Costa <higorvaz@gmail.com>
2022-11-07 20:53:07 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,27 @@
+from flask import Flask 
+from flask import render_template
+from flask import request
+
+from
+app = Flask(__name__)
+
+@app.route('/', methods=["POST", "GET"])
+def home():
+  if request.method == "POST":
+    val1 = int(request.form["field1"])
+    val2 = int(request.form["field2"])
+
+
+
+    if (request.form.get("add")):
+      res = val1 + val2
+      return render_template('index.html', val=res)
+    if (request.form.get("sub")):
+      res = val1 - val2
+      return render_template('index.html', val=res)
+
+    return render_template('index.html')
+  else:
+    return render_template('index.html', val="na")
+
+app.run(host="0.0.0.0", debug=True)
